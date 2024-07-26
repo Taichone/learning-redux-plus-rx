95135f461c197200a9f9c3e189660eb7e1d89207
Taichone <taichone.dev@gmail.com>
2024-07-26 16:33:27 +0900

    Initial Commit

---
+++ LearningRedux+Rx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningRedux+Rx
+//
+//  Created by Taichi on 2024/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningRedux+Rx/LearningRedux_RxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningRedux_RxApp.swift
+//  LearningRedux+Rx
+//
+//  Created by Taichi on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningRedux_RxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
